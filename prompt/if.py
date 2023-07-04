d2606b0d8428e63f267faf84dc675206acb8aa4c
Dipu Singha <hunabopa@gmail.com>
2023-07-04 10:06:57 +0600

    Some modification on prompt.py file.

---
+++ prompt/if.py
@@ -0,0 +1,10 @@
+qualification1 = "Python"
+qualification2 = "JavaScript"
+qualification3 = "Java"
+q1 = qualification1
+q2 = qualification2
+q3 = qualification3
+if q1 == "Python":
+    print("You are chosen by Google.")
+else :
+    print("You are chosen by Microsoft")
